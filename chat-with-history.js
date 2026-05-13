03804f62828fdcc80f917415da5d5bcb620e732f
pradipta <ppradipta65@gmail.com>
2026-05-13 12:52:02 +0530

    chat

--- chat-with-history.js
+++ chat-with-history.js
@@ -1,34 +1,66 @@
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import { GoogleGenAI } from "@google/genai";
 dotenv.config()
 
-const context = [{
+const openAiContext = [{
     role: 'system',
     content: 'keep answer short and simple'
-}]
+}];
 
-async function openAIAnswer(data) {
+const genAIContext = [{
+    role: 'user',
+    parts: [{ text: 'keep answer short and simple' }]
+}];
+
+async function openAIAnswerWithHistory(data) {
 
     const client = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY || ''
     });
 
-    context.push({
+    openAiContext.push({
         role: 'user',
         content: data
     })
 
     const response = await client.responses.create({
         model: "gpt-5.5",
-        input: data
+        content: openAiContext
     });
 
-    context.push({
+    openAiContext.push({
         role: 'assistant',
         content: response.output_text
     })
 
     return response.output_text;
+}
+
+async function googleGenAIAnswerrWithHistory(data) {
+    try {
+        const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+
+        genAIContext.push({
+            role: 'user',
+            parts: [{ text: data }]
+        })
+
+        const response = await ai.models.generateContent({
+            model: 'gemini-1.5-pro',
+            contents: genAIContext
+        });
+
+        genAIContext.push({
+            role: 'model',
+            parts: [{ text: response.text }]
+        })
+
+        return response.text;
+    } catch (error) {
+        console.error("Error calling Google GenAI:", error);
+        throw error;
+    }
 }
 
 process.stdout.write("Ask question : ");
@@ -37,7 +69,7 @@ process.stdin.on("data", async (data) => {
         process.exit()
     }
     else {
-        const output = await openAIAnswer(data.toString().trim());
+        const output = await googleGenAIAnswerrWithHistory(data.toString().trim());
         console.log(output);
     }
 })
